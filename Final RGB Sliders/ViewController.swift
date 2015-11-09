2035d24511060c4129c11316a2293271611769ac
Audrey Brown <abrown@abrown-2017.local>
2015-11-09 08:38:46 -0500

    Initial Commit

---
+++ Final RGB Sliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final RGB Sliders
+//
+//  Created by Audrey Brown on 11/9/15.
+//  Copyright © 2015 Audrey Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
